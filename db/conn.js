a047a5873aa470cb447defe3db753f28c87377bb
Prathmesh Nikam <prathameshnikam09@gmail.com>
2023-03-05 09:16:34 +0530

    remove connection uri

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const DATABASE = process.env.DATABASE;
-// const DATABASE = "mongodb+srv://prathmesh:prathmeshnikam@learnsitecluster.nwbg9cf.mongodb.net/useservice?retryWrites=true&w=majority";
 mongoose
   .connect(DATABASE)
   .then(() => {
